6675909189777b02e6dea8567ee5ada450176c4f
Tucker Polomik <tuckerpo@buffalo.edu>
2019-02-27 20:47:03 -0500

    wut

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include "chip8.hpp"
 #include "gui.hpp"
 #include <SDL2/SDL.h>
+#include <memory>
 #ifdef _WIN32
 #define SDL_MAIN_HANDLED
 #endif
@@ -8,12 +9,12 @@
 int main(int argc, char *argv[]) 
 {
 	/* GUI */
-	if (!SDL_Init(SDL_INIT_EVERYTHING)) 
+	if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO) != 0) 
 	{	
 		c8gui::error("Error initializing SDL2:", SDL_GetError());
 	}
 
-	SDL_Window* c8window = SDL_CreateWindow(c8gui::window_title, SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, c8gui::w, c8gui::h, SDL_WINDOW_OPENGL);
+	SDL_Window *c8window = SDL_CreateWindow(c8gui::window_title, SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, c8gui::w, c8gui::h, SDL_WINDOW_OPENGL);
 	if (c8window == nullptr) c8gui::error("Window initialization error: ", SDL_GetError());
 
     Chip8 c8 = Chip8();
