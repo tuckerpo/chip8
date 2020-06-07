c22ca9444626e8433e9cba2657e9d18d5ea0da3a
thenergal <tuckerpo@buffalo.edu>
2020-06-07 00:37:56 -0400

    Remove: dead code, vim tags files. Update: Windows build batch files to reflect removed code.

--- include/Opcode.hpp
+++
@@ -1,5 +0,0 @@
-
-typedef uint8_t byte;
-class Opcode {
-    constexpr static byte nibblemask = 0xF;
-};
--- src/chip8factory.cpp
+++
@@ -1,6 +0,0 @@
-#include "chip8factory.hpp"
-
-std::shared_ptr<Chip8> Chip8Factory::createChip8() {
-	return std::make_shared<Chip8>(Chip8());
-
-}
